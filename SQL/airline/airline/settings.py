11bda36c4bddd85608048c0d90e8380d6f68b523
Youliya Bratishko <youliyav@gmail.com>
2022-02-15 13:40:27 +0300

    Add project airline

--- SQL/airline/airline/settings.py
+++ SQL/airline/airline/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3su8^xp5@^-rj^$*oqaf&#+#ercc_eugahp(+23=w1eg@dka#e'
+SECRET_KEY = 'django-insecure-%ly6@yqz4i!oxk3qy&yzn_8kw9s%*tl7dh*^49tck35jffd4bh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
